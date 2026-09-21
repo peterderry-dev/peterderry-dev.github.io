a3e8749dc0d7c35800affd72ced3dc22a2ee8cd5
peterderry-dev <peter.derry@sarahtreed.org>
2026-09-21 20:05:07 +0000

    yeaaaa bby

--- fsd-projects/platformer/platformer.js
+++ fsd-projects/platformer/platformer.js
@@ -29,11 +29,24 @@ $(function () {
     // TODO 1 - Enable the Grid
     toggleGrid();
 
-createPlatform(200, 650, 200, 30);
-createPlatform(600, 550, 100, 30);
-createPlatform(900, 600, 400, 30);
-createPlatform(150, 450, 200, 30);
-createPlatform(1000, 480, 200, 30);
+createPlatform(200, 650, 200, 30, "blue");
+createPlatform(600, 550, 100, 30, "blue");
+createPlatform(900, 600, 400, 30, "blue");
+createPlatform(150, 450, 200, 30, "blue");
+createPlatform(1000, 480, 200, 30, "blue");
+createPlatform(450, 400, 250, 30, "blue");
+createPlatform(1000, 400, 30, 100, "blue");
+createPlatform(750, 300, 200, 30, "blue");
+
+createCollectable("steve", 800, 250);
+createCollectable("steve", 200, 410);
+createCollectable("steve", 1100, 440);
+
+createCannon("bottom", 500, 740);
+createCannon("bottom", 750, 740);
+createCannon("top", 1180, 750);
+createCannon("right", 420, 750);
+createCannon("left", 210, 750);
 
 
 
